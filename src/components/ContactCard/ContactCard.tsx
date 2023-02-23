e54e0f495828e22b66a6531dec78e745c35a0274
Amaury Chalot <amaurychalot@gmail.com>
2023-02-23 20:03:33 +0100

    feat: added text content to profil card

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -1,29 +1,38 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material";
 
 export const ContactCard = () => {
   const theme = useTheme();
 
   return (
-    <Card
+    <Box
       sx={{
-        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: theme.palette.background.paper,
+        borderRadius: "10px",
         padding: "1rem",
+        width: "100%",
+        boxSizing: "border-box",
+        gap: "1rem",
       }}
     >
       <Box
         sx={{
           width: "200px",
-          height: "200px",
+          height: "250px",
           overflow: "hidden",
-          //borderRadius: "10px",
-          borderRadius: "9999px",
-          border: "2px solid " + theme.palette.common.white,
+          borderRadius: "10px",
+          //borderRadius: "9999px",
+          //border: "2px solid " + theme.palette.common.white,
           display: "flex",
           alignItems: "flex-start",
           justifyContent: "center",
+          flexGrow: 0,
         }}
       >
         <img
@@ -31,6 +40,27 @@ export const ContactCard = () => {
           src="/images/pp.png"
         />
       </Box>
-    </Card>
+      <Box
+        sx={{
+          flexGrow: 1,
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          boxSizing: "border-box",
+        }}
+      >
+        <Typography
+          component="h1"
+          sx={{
+            color: theme.palette.text.primary,
+            fontSize: theme.typography.h4.fontSize,
+            fontWeight: 800,
+            fontFamily: theme.typography.fontFamily,
+          }}
+        >
+          AMAURY CHALOT
+        </Typography>
+      </Box>
+    </Box>
   );
 };
