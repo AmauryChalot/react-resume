7044ec192e370187d2d2a5ae4b499fbda5db0f4d
Amaury Chalot <amaurychalot@gmail.com>
2023-04-15 16:56:29 +0200

    refactor: hook for scrolled section in custom card

---
+++ src/components/Card/CustomCard.hook.ts
@@ -0,0 +1,51 @@
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+
+interface useScrollableRefProps {
+  containerRef: React.MutableRefObject<HTMLDivElement | null>;
+  index: number;
+}
+
+export const useScrollableRef = (
+  containerRef: React.MutableRefObject<HTMLDivElement | null>,
+  index: number,
+  setScrolledSections: Dispatch<SetStateAction<number>>,
+) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [refIsVisible, setRefIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    ref.current?.focus();
+    const handleScroll = (e: Event) => {
+      if (
+        containerRef?.current &&
+        containerRef.current?.scrollTop >= 0 &&
+        containerRef.current?.scrollTop <=
+          containerRef.current?.scrollHeight / 64
+      ) {
+        setRefIsVisible(false);
+      } else if (
+        containerRef?.current &&
+        ref.current &&
+        ref.current.offsetTop <
+          containerRef.current?.scrollTop +
+            containerRef?.current?.clientHeight / 2.5
+      ) {
+        setScrolledSections(index);
+        setRefIsVisible(true);
+      } else {
+        setRefIsVisible(false);
+      }
+    };
+    if (containerRef?.current)
+      containerRef.current.addEventListener('scroll', handleScroll);
+    return () => {
+      if (containerRef?.current)
+        containerRef.current.removeEventListener('scroll', handleScroll);
+    };
+  }, [containerRef]);
+
+  return {
+    ref,
+    refIsVisible,
+  };
+};
--- src/components/Card/CustomCard.tsx
+++ src/components/Card/CustomCard.tsx
@@ -1,5 +1,6 @@
 import { Card, Typography } from '@mui/material';
 import React from 'react';
+import { useScrollableRef } from './CustomCard.hook';
 import { CustomCardProps } from './CustomCard.models';
 import {
   sxCustomCardContainer,
@@ -10,54 +11,24 @@ import {
 } from './CustomCard.styles';
 
 export const CustomCard = (props: CustomCardProps) => {
-  const [scrolledSections, setScrolledSections] = props.scrolledSectionsState;
+  const [_, setScrolledSections] = props.scrolledSectionsState;
 
-  const myRef = React.useRef<HTMLDivElement | null>(null);
-  const [myRefIsVisible, setMyRefIsVisible] = React.useState<boolean>(false);
-
-  React.useEffect(() => {
-    myRef.current?.focus();
-    const handleScroll = (e: Event) => {
-      if (
-        props.containerRef?.current &&
-        props.containerRef.current?.scrollTop >= 0 &&
-        props.containerRef.current?.scrollTop <=
-          props.containerRef.current?.scrollHeight / 64
-      ) {
-        setMyRefIsVisible(false);
-      } else if (
-        props.containerRef?.current &&
-        myRef.current &&
-        myRef.current.offsetTop <
-          props.containerRef.current?.scrollTop +
-            props.containerRef?.current?.clientHeight / 2.5
-      ) {
-        setScrolledSections(props.index);
-        setMyRefIsVisible(true);
-      } else {
-        setMyRefIsVisible(false);
-      }
-    };
-    if (props.containerRef?.current)
-      props.containerRef.current.addEventListener('scroll', handleScroll);
-    return () => {
-      if (props.containerRef?.current)
-        props.containerRef.current.removeEventListener('scroll', handleScroll);
-    };
-  }, [props.containerRef]);
+  const { ref, refIsVisible } = useScrollableRef(
+    props.containerRef,
+    props.index,
+    setScrolledSections,
+  );
 
   return (
-    <Card sx={sxCustomCardContainer} id={props.id} ref={myRef}>
+    <Card sx={sxCustomCardContainer} id={props.id} ref={ref}>
       <Typography
-        sx={myRefIsVisible ? sxCustomCardTitleHighlighted : sxCustomCardTitle}
+        sx={refIsVisible ? sxCustomCardTitleHighlighted : sxCustomCardTitle}
       >
         {props.title}
       </Typography>
       <Typography
         component="div"
-        sx={
-          myRefIsVisible ? sxCustomCardContentHighlighted : sxCustomCardContent
-        }
+        sx={refIsVisible ? sxCustomCardContentHighlighted : sxCustomCardContent}
       >
         {props.content}
       </Typography>
